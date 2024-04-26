9e2f4bb7bf304d022ade1b9505d305a1447f0ab4
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-04-26 16:24:23 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { Container, Section, StatisticsList, BlogCard } from "components";
-import Heading from "components/Heading/Heading";
-import article from "data/article.json";
-import stats from "data/stats.json";
+// import { Container, Section, StatisticsList, BlogCard } from "components";
+// import Heading from "components/Heading/Heading";
+// import article from "data/article.json";
+// import stats from "data/stats.json";
+
+import { BlogCard } from "./components/BlogCard/BlogCard";
+import article from "./components/data/article.json"
 
 function App() {
   return (
-    <Section>
-      <Container>
-        <Heading title="Blog Card"/>
-        <BlogCard {...article} />
-        <Heading title="Statistic List"/>
-        <StatisticsList stats={stats} title="Statistics" />
-      </Container>
-    </Section>
+    <>
+    <BlogCard name={article.name} avatar={article.avatar}/>
+    </>
+    // <Section>
+    //   <Container>
+    //     <Heading title="Blog Card"/>
+    //     <BlogCard {...article} />
+    //     <Heading title="Statistic List"/>
+    //     <StatisticsList stats={stats} title="Statistics" />
+    //   </Container>
+    // </Section>
   );
 }
 
--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,12 +1,16 @@
-export const BlogCard = ({ name, avatar, poster, tag, title, description }) => {
+export const BlogCard = ({name, avatar}) => {
+  
     return (
       <div>
-        <h2>name {name}</h2>
+        BlogCard
+        <h2>Name{name}</h2>
+        <img src={avatar} alt={name} />
+        {/* <h2>name {name}</h2>
         <img src={avatar} alt={name} />
         <p>tag{tag}</p>
         <img src={poster} alt={tag} />
         <p>title {title}</p>
-        <p>description {description}</p>
+        <p>description {description}</p> */}
       </div>
     );
   };
